0f1f5898f3f95a3aeb932f350ccf0dd480603120
josue david kennedy <104701497+programfive@users.noreply.github.com>
2025-12-30 15:09:33 -0400

    astro error send emails

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,14 @@ import vercel from "@astrojs/vercel/serverless";
 export default defineConfig({
   site: 'https://josuekennedy.com',
   output: 'server',
-  adapter: vercel(),
+  adapter: vercel({
+    maxDuration: 30,
+  }),
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
+    ssr: {
+      external: ['resend']
+    }
   },
 });
